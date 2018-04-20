2168aed1ad3c04a9caa860c82c33e8121ae0c2b1
hkhv <746392783@qq.com>
2018-04-20 11:35:44 +0800

    Update

--- src/pages/asset/detail/detail.component.ts
+++ src/pages/asset/detail/detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+    selector: 'asset-detail',
     templateUrl: 'detail.component.html'
 })
 export class AssetDetailComponent implements OnInit {
--- src/pages/asset/list/list.component.ts
+++ src/pages/asset/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../../../core';
 
 @Component({
+    selector: 'asset-list',
     templateUrl: 'list.component.html'
 })
 export class AssetListComponent implements OnInit {
