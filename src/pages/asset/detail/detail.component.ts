bd0ecf8b243ae3b1d6885caa0538b8d7e972b043
hkhv <746392783@qq.com>
2018-04-20 16:36:58 +0800

    Update asset-detail

--- src/pages/asset/detail/detail.component.ts
+++ src/pages/asset/detail/detail.component.ts
@@ -1,11 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScroll } from 'ionic-angular';
+import {
+    WalletHomeComponent
+} from '../../../pages';
 
 @Component({
     selector: 'asset-detail',
     templateUrl: 'detail.component.html'
 })
 export class AssetDetailComponent implements OnInit {
-    constructor() { }
-
-    public ngOnInit() { }
+    constructor() {}
+    public items: any[]
+    public tab1: any
+    public tab2: any
+    public ngOnInit() {
+        this.tab1 = WalletHomeComponent 
+        this.tab2 = WalletHomeComponent 
+        this.items = [];
+        for (let i = 0; i < 5; i++) {
+            this.items.push( this.items.length );
+        }
+      
+     }
+   
+    public doInfinite(InfiniteScroll): Promise<any> {
+        console.log('Begin async operation');
+        this.items = [];
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                for (let i = 0; i < 5; i++) {
+                    this.items.push(this.items.length);
+                }
+                console.log('Async operation has ended');
+                InfiniteScroll.complete();
+            }, 300);
+        })
+    }
 }
