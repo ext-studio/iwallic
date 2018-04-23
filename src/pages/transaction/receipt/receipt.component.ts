fd2d0c38c2f0f1cd3e6577c37fabc7c935483d57
hkhv <746392783@qq.com>
2018-04-23 16:48:53 +0800

    Update

--- src/pages/transaction/receipt/receipt.component.ts
+++ src/pages/transaction/receipt/receipt.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import QrCodeWithLogo from 'qr-code-with-logo';
+import { Storage } from '@ionic/storage';
 @Component({
     selector: 'transaction-receipt',
     templateUrl: 'receipt.component.html',
 })
 export class TxReceiptComponent implements OnInit {
-    constructor() { }
+    constructor(
+        private storage: Storage
+    ) { }
 
     public ngOnInit() {
         this.getQRCode();
     }
-    public  getQRCode() {
+    public  getQRCode():  Promise<any> {
         const qrcode = document.getElementById('qrcode');
         QrCodeWithLogo.toImage({
             image: qrcode,
