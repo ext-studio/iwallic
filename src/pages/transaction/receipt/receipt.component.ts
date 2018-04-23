275522b974b7059444607c1711fa9e2dc4388096
hkhv <746392783@qq.com>
2018-04-23 16:30:03 +0800

    Update receipt

--- src/pages/transaction/receipt/receipt.component.ts
+++ src/pages/transaction/receipt/receipt.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import QrCodeWithLogo from 'qr-code-with-logo';
 @Component({
     selector: 'transaction-receipt',
     templateUrl: 'receipt.component.html',
@@ -7,5 +7,20 @@ import { Component, OnInit } from '@angular/core';
 export class TxReceiptComponent implements OnInit {
     constructor() { }
 
-    public ngOnInit() { }
+    public ngOnInit() {
+        this.getQRCode();
+    }
+    public  getQRCode() {
+        const qrcode = document.getElementById('qrcode');
+        QrCodeWithLogo.toImage({
+            image: qrcode,
+            content: 'http://blog.cloudself.cn',
+            width: 200,
+            logo: {
+                src: 'assets/asset/qrcode_logo.png',
+                radius: 8
+            }
+        });
+        return ;
+    }
 }
