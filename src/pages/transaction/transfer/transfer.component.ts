8bc7c350361eb2f8ec42808e50ed4dc32dadbfe5
hkhv <746392783@qq.com>
2018-04-24 14:43:16 +0800

    Update transfer

--- src/pages/transaction/transfer/transfer.component.ts
+++ src/pages/transaction/transfer/transfer.component.ts
@@ -5,7 +5,18 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: 'transfer.component.html',
 })
 export class TxTransferComponent implements OnInit {
+    public balance: number;
+    public isfocus: boolean = false;
     constructor() { }
 
-    public ngOnInit() { }
+    public ngOnInit() {
+        this.balance = 0;
+    }
+
+    public focusNum() {
+        this.isfocus = true;
+    }
+    public blurNum() {
+        this.isfocus = false;
+    }
 }
