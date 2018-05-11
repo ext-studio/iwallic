06b5a154d3fdcd4c35ce75ed4ddf507117092368
yitimo <admin@yitimo.com>
2018-05-11 14:55:54 +0800

    fix export

--- src/neo/index.ts
+++ src/neo/index.ts
@@ -4,5 +4,7 @@ export const create = Neon.create;
 export { RPCService } from './rpc.service';
 export { TransactionService } from './transaction.service';
 export { UtilService, ASSET } from './util.service';
-export { WalletService, Wallet } from './wallet.service';
+export { WalletService } from './wallet.service';
+export * from './models/wallet';
+export * from './models/transaction';
 export { NEOModule } from './neo.module';
