cc31aed2d388a1e4c7603ae1ccf71698b31333c2
hkhv <746392783@qq.com>
2018-06-06 16:56:59 +0800

    Update

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+if ((navigator as any).splashscreen) {
+    (navigator as any).splashscreen.show();
+}
 
-(navigator as any).splashscreen.show();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
