4a106a10f7f0ca51b9a52c53ac8242a1a623ebaa
yitimo <admin@yitimo.com>
2018-10-31 11:40:39 +0800

    simple asset manage

--- src/app/asset/list/list.component.ts
+++ src/app/asset/list/list.component.ts
@@ -4,6 +4,7 @@ import { WalletService, ASSET, BlockService } from '../../neo';
 import { BalanceState, DialogService } from '../../core';
 import { Subscription } from 'rxjs';
 import { Block } from 'app/neo/models/block';
+import { Storage } from '@ionic/storage';
 
 @Component({
     templateUrl: 'list.component.html',
@@ -22,7 +23,8 @@ export class ListComponent implements OnInit {
         private wallet: WalletService,
         private balance: BalanceState,
         private dialog: DialogService,
-        private block: BlockService
+        private block: BlockService,
+        private storage: Storage
     ) {}
 
     public refresh(event) {
@@ -41,6 +43,10 @@ export class ListComponent implements OnInit {
         });
     }
 
+    public manage() {
+        this.router.navigate(['/asset/manage']);
+    }
+
     ngOnInit(): void {
         this.$block = this.block.listen().subscribe((res: Block) => {
             console.log(`block ${res.latestHeight} arrived`);
@@ -48,6 +54,7 @@ export class ListComponent implements OnInit {
         });
         this.$balance = this.balance.listen(this.wallet.address).subscribe((res: any[]) => {
             this.list = res;
+            this.resolveWatch();
             const tryGet = res.find((e) => e.assetId === ASSET.NEO);
             this.neoValue = tryGet && tryGet.balance || 0;
         });
@@ -59,5 +66,17 @@ export class ListComponent implements OnInit {
         this.$block.unsubscribe();
         this.$balance.unsubscribe();
         this.$error.unsubscribe();
+    }
+
+    private resolveWatch() {
+        this.storage.get('asset_watch').then((res) => {
+            if (res instanceof Array && res.length) {
+                res.forEach((e) => {
+                    if (this.list.findIndex((i) => i['asset_id'] == e['asset_id']) < 0) {
+                        this.list.push(e);
+                    }
+                });
+            }
+        });
     }
 }
--- src/app/asset/manage/manage.component.ts
+++ src/app/asset/manage/manage.component.ts
@@ -1,21 +1,109 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService, DialogService } from 'app/core';
+import { Storage } from '@ionic/storage';
+import { Refresher } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
     templateUrl: 'manage.component.html',
     styleUrls: ['manage.component.scss']
 })
 export class ManageComponent implements OnInit {
-    constructor() { }
+    public watching = [];
+    public list = [];
+    public totalPage = 1;
+
+    public page = 1;
+    private take = 15;
+    private nomore = false;
+
+    constructor(
+        private http: HttpService,
+        private storage: Storage,
+        private dialog: DialogService
+    ) {
+        this.storage.get('asset_watch').then((res) => {
+            if (res instanceof Array && res.length) {
+                this.watching = res;
+            }
+        });
+        this.dialog.loader('Loading').then((loader) => {
+            this.fetch(1).subscribe((res) => {
+                this.list = res;
+                loader.dismiss();
+            }, (err) => {
+                this.dialog.toast('Load failed.');
+                console.log(err);
+                loader.dismiss();
+            });
+        });
+    }
 
     ngOnInit(): void { }
 
-    private toggle() {
-
+    public toggle(e: any, i: number) {
+        const index = this.watching.findIndex((el) => el['asset_id'] == this.list[i].asset_id);
+        if (index >= 0) {
+            this.watching.splice(index);
+        } else {
+            this.watching.push({asset_id: this.list[i].asset_id, balance: 0, name: this.list[i].name, symbol: this.list[i].symbol});
+        }
+        this.storage.set('asset_watch', this.watching);
     }
-    private refresh() {
-
+    public refresh(event) {
+        this.fetch(1).subscribe((res) => {
+            this.list = res;
+            event.target.complete();
+        }, (err) => {
+            this.dialog.toast('Load failed.');
+            event.target.cancel();
+            console.log(err);
+        });
     }
-    private more() {
-
+    public more(event) {
+        if (this.nomore) {
+            event.target.complete();
+            return;
+        }
+        this.fetch(this.page+1).subscribe((res) => {
+            this.list = this.list.concat(res);
+            event.target.complete();
+        }, (err) => {
+            this.dialog.toast('Load failed.');
+            console.log(err);
+            event.target.complete();
+        });
+    }
+    public trash() {
+        this.dialog.confirm(
+            'Sure to clean all watching asset?(Just hide from home page, no effect for balance)',
+            'Notice',
+            'Clean',
+            'Cancel'
+        ).then((confirm) => {
+            if (confirm) {
+                this.dialog.loader().then((loader) => {
+                    this.storage.remove('asset_watch');
+                    this.watching = [];
+                    this.fetch(1).subscribe((res) => {
+                        loader.dismiss();
+                        this.list = res;
+                    });
+                    this.dialog.toast('Done!');
+                });
+            }
+        })
+    }
+    private fetch(page: number): Observable<any[]> {
+        this.page = page;
+        return this.http.get(`/client/assets/list?page=${page}&page_size=${this.take}`).pipe(map((res) => {
+            this.totalPage = res.pages;
+            this.nomore = res.page == this.totalPage || !res.items.length;
+            return res.items.map((e) => {
+                e['chosen'] = this.watching.findIndex((i) => i['asset_id'] == e['asset_id']) >= 0;
+                return e;
+            });
+        }));
     }
 }
